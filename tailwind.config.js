050edfb086a55d27208fe1d204296df8cced8d66
Akhi7272 <akhifincou@stud.cou.ac.bd>
2024-08-22 13:23:02 +0600

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': "url('./C:\assignment-03\assets\bg.jpg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
+  }
+}
