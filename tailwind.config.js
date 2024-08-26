64b0934f0dead9a4500d452056b3eceb87b2d4a6
Akhi7272 <akhifincou@stud.cou.ac.bd>
2024-08-26 17:15:03 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero': "url('./C:\assignment-03\assets\bg.jpg')",
+        'hero': "url('.\assets\bg.jpg')",
         'footer-texture': "url('/img/footer-texture.png')",
       }
     }
